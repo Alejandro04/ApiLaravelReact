1e060a811f208c3fda9d5a2b3b28a242f0826f08
Gladys Chacón de Roa <Gladys Chacn de Roa>
2017-03-18 19:29:19 -0700

    pruebas, consumir api con axios, fetch y ajax

--- front/components/lista-notas/index.jsx
+++ front/components/lista-notas/index.jsx
@@ -1,21 +1,60 @@
 import React from 'react'
+import axios from 'axios';
+import $ from 'jquery';
 
 class ListaNotas extends React.Component {
 
   constructor() {
     super()
-    this.state = { notas: [] }
+     this.state = {
+        notas: []
+      }
   }
 
   componentWillMount() {
+
+
+  $.ajax({
+      url: 'http://localhost:8000/notas',
+      dataType: 'json',
+      type: 'GET',
+      success: function(data) {
+        this.setState({notas: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error('error');
+      }.bind(this)
+   });
+
+/*
+
+  axios.get('http://localhost:8000/notas')
+  .then(function (response) {
+      const notas = response.data.notas
+      this.setState({notas: notas});
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+  */
+
+
+  /*
     fetch('http://localhost:8000/notas')
       .then((response) => {
         return response.json()
       })
       .then((notas) => {
-        this.setState({notas: notas})
+       if(notas != null)
+       {
+          this.setState({notas: notas})
+       }
       })
+        */
+
   }
+
 
   render() {
     console.log(this.state.notas); //Se trae el estado inicial (por eso rebota) y luego el estado actualizado
